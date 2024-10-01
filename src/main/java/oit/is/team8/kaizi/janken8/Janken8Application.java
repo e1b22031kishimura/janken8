ba372db5f52e0695ea3951124f1d1b776b9cde3d
e1b22001 <e1b22001@oit.ac.jp>
2024-10-01 14:58:15 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team8/kaizi/janken8/Janken8Application.java
@@ -0,0 +1,13 @@
+package oit.is.team8.kaizi.janken8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken8Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team8/kaizi/janken8/Janken8ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team8.kaizi.janken8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
